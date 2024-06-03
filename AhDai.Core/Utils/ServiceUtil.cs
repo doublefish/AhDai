4c9852bc0e3a52996712ba9e98c79f62413297c9
doublefish <doublefish1989@gmail.com>
2024-06-03 09:38:23 +0800

    去除不通用的服务实例，打包

--- AhDai.Core/Utils/ServiceUtil.cs
+++ AhDai.Core/Utils/ServiceUtil.cs
@@ -1,10 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
 using System;
-using System.Linq;
 
 namespace AhDai.Core.Utils;
 
@@ -21,28 +16,6 @@ public static class ServiceUtil
     /// Configuration
     /// </summary>
     public static IConfiguration Configuration { get; private set; } = default!;
-	/// <summary>
-	/// HttpContextAccessor
-	/// </summary>
-	public static IHttpContextAccessor HttpContextAccessor { get; private set; } = default!;
-    /// <summary>
-    /// HostEnvironment
-    /// </summary>
-    public static IHostEnvironment HostEnvironment { get; private set; } = default!;
-    /// <summary>
-    /// HostEnvironment
-    /// </summary>
-    public static IWebHostEnvironment WebHostEnvironment { get; private set; } = default!;
-
-    /// <summary>
-    /// 初始化
-    /// </summary>
-    /// <param name="services"></param>
-    /// <param name="configuration"></param>
-    public static void Init(IServiceCollection services, IConfiguration configuration)
-	{
-		Init(services.BuildServiceProvider(), configuration);
-	}
 
 	/// <summary>
 	/// 初始化
@@ -53,23 +26,5 @@ public static class ServiceUtil
 	{
 		Services = services;
 		Configuration = configuration;
-		HttpContextAccessor = services.GetRequiredService<IHttpContextAccessor>();
-		HostEnvironment = services.GetRequiredService<IHostEnvironment>();
-		WebHostEnvironment = services.GetRequiredService<IWebHostEnvironment>();
-	}
-
-	/// <summary>
-	/// 获取当前Token数据
-	/// </summary>
-	/// <returns></returns>
-	public static Models.TokenData? GetCurrentTokenData()
-	{
-		var httpContext = HttpContextAccessor.HttpContext;
-		if (httpContext != null && httpContext.User != null)
-		{
-			var jwtService = Services.GetRequiredService<Services.IBaseJwtService>();
-			return jwtService.ToTokenData(httpContext.User.Claims.ToArray());
-		}
-		return null;
 	}
 }
